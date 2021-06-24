ed545b42b11d1f3f160e80c2f11fd200e6dccb04
Niels Delestinne <niels.delestinne@acerta.be>
2021-06-24 23:36:54 +0200

    Clean-up controller + 50.000 cats

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,31 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
-import { Cat, Gender } from '@juge/type-api';
+import { Cat } from '@juge/type-api';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  private cats = this.generateCats(50000);
-
   @Get()
   getData(): Cat[] {
-    return this.cats;
-  }
-
-  private generateCats(amount = 50): Cat[] {
-    const cats: Cat[] = [];
-    for(let i = 0; i < amount; i++) {
-      cats.push({
-        name: 'Jim',
-        age: i,
-        isKeptIndoor: true,
-        gender: Gender.MALE
-
-      })
-    }
-    console.log('ready');
-    return cats;
+    return this.appService.getAllCats_notOptimized();
   }
 }
